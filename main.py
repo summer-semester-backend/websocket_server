61a7d6c4e1cdc27b06a6905d0fdf69c3fb615491
WeiXia <1114891937@qq.com>
2022-08-08 10:46:30 +0800

    能用了

--- main.py
+++ main.py
@@ -16,28 +16,32 @@ def broadcast_thread():
     # start_server = websockets.serve(unknown, '127.0.0.1', 8001)
     # asyncio.get_event_loop().run_until_complete(start_server)
     # asyncio.get_event_loop().run_forever()
-    start_server = websockets.serve(unknown, "127.0.0.1", 8001)
+    start_server = websockets.serve(unknown, "0.0.0.0", 8001)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
 
 async def unknown(websocket):
-    await websocket.send({'result': 0, 'message': '正在连接...'})
+    await websocket.send(json.dumps({'result': 0, 'message': '正在连接...'}))
     async for message in websocket:
+        print("接收到: "+message)
         data = json.loads(message)
-        print(data)
+        # print(data)
         if data['operation'] == 'register':  # 连接
-            websocket.send(json.dumps({'result': 0, 'message': '已连接到同步编辑服务'}))
+            await websocket.send(json.dumps({'result': 0, 'message': '已连接到同步编辑服务'}))
             fileID = data['fileID']
             userID = data['userID']
             if fileID not in FILES:
                 FILES[fileID] = {}
             dic = FILES[fileID]
-            broad = {'result': 0, 'message': '用户' + str(userID) + "已加入编辑"}
-            await asyncio.wait([ws.send(json.dumps(broad)) for ws in dic.values()])
-            FILES[data['fileID']][data['userID']] = websocket
-        elif data['operation'] == 'change':
-            pass
+            for that_userID in dic:
+                await websocket.send(json.dumps({
+                    'operation': 'register',
+                    'userID': that_userID,
+                    'fileID': fileID,
+                }))
+            # broad = {'result': 0, 'message': '用户' + str(userID) + "已加入编辑"}
+            FILES[fileID][userID] = websocket
         elif data['operation'] == 'leave':  # 断开连接
             # websocket.send(json.dumps({'result': 0, 'message': '已连接到同步编辑服务'}))
             fileID = data['fileID']
@@ -45,6 +49,11 @@ async def unknown(websocket):
             dic = FILES[fileID]
             broad = {'result': 0, 'message': '用户' + str(userID) + "已离开"}
             FILES[fileID].pop(userID)
-            await asyncio.wait([ws.send(json.dumps(broad)) for ws in dic.values()])
+        for that_userID in dic:
+            if that_userID != data['userID']:
+                await dic[that_userID].send(message)
+        # if len(dic.values()) > 0:
+        #     print("向{}个用户发起转发".format(len(dic.values())))
+        #     await asyncio.wait([ws.send(message) for ws in dic.values()])
 
 broadcast_thread()
